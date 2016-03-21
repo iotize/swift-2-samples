a327a8ac2e53bf3cb9a13d8de15319c348b1af49
Ryan Davies <mail@ryanjdavies.com>
2016-03-21 22:42:59 +0000

    Initial Commit

---
+++ readLine/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  readLine
+//
+//  Created by Ryan Davies on 21/03/2016.
+//  Copyright © 2016 Ryan Davies. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
